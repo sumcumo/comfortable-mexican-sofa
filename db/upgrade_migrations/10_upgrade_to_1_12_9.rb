79108267a20f302dd95fc4b1b11449bf793145fa
Matthias Bauer <moeffju@moeffju.net>
2015-03-23 17:22:35 +0100

    Extend migration

--- db/upgrade_migrations/10_upgrade_to_1_12_9.rb
+++ db/upgrade_migrations/10_upgrade_to_1_12_9.rb
@@ -1,9 +1,11 @@
 class UpgradeTo1129 < ActiveRecord::Migration
   def self.up
     add_column :comfy_cms_pages, :is_withdrawn, :boolean, :default => false
+    add_column :comfy_cms_pages, :newest_draft_timestamp, :datetime, :null => true
   end
 
   def self.down
     remove_column :comfy_cms_pages, :is_withdrawn, :boolean
+    remove_column :comfy_cms_pages, :newest_draft_timestamp, :datetime
   end
 end
