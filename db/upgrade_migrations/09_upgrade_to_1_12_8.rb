61344ed7bb985827082badadbffbbf09580dcacf
Matthias Bauer <moeffju@moeffju.net>
2015-03-23 09:30:16 +0100

    Fix migration naming

--- db/upgrade_migrations/09_upgrade_to_1_12_8.rb
+++ db/upgrade_migrations/09_upgrade_to_1_12_8.rb
@@ -1,4 +1,4 @@
-class UpgradeTo1121 < ActiveRecord::Migration
+class UpgradeTo1128 < ActiveRecord::Migration
   def self.up
     add_column :comfy_cms_pages, :last_published_revision_id, :integer, :null => true
     add_column :comfy_cms_pages, :scheduled_revision_id, :integer, :null => true
